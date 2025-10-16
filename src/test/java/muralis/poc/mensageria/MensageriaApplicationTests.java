845969b112f274b6f561dfc951366b13dd841886
matheus.graciliano <matheus.graciliano@muralis.com.br>
2025-10-16 09:31:39 -0300

    feat: config do rabbitmq e postgres no compose

---
+++ src/test/java/muralis/poc/mensageria/MensageriaApplicationTests.java
@@ -0,0 +1,13 @@
+package muralis.poc.mensageria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MensageriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
